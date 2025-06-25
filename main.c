97cef9489d9dbee53294f87bda38b62897a6588c
DonizeteVida <donizetevida1998@outlook.com>
2025-06-25 23:22:42 +0200

    Read/render image, add scroll wheel opacity functionality

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 
-#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
 
-/* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static SDL_Texture *texture = NULL;
 
-/* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+    SDL_Surface *surface = NULL;
 
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
@@ -18,52 +18,64 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0, &window, &renderer))
+    if (!SDL_SetHint(SDL_HINT_MAIN_CALLBACK_RATE, "15"))
+    {
+        SDL_Log("Couldn't set framerate: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!(surface = IMG_Load("sample.jpeg")))
+    {
+        SDL_Log("Couldn't load image: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer(
+            "translucent",
+            surface->w,
+            surface->h,
+            SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_TRANSPARENT,
+            &window,
+            &renderer))
     {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_SetWindowOpacity(window, 0.5f))
+    if (!(texture = SDL_CreateTextureFromSurface(renderer, surface)))
     {
-        SDL_Log("Couldn't set window opacity: %s", SDL_GetError());
+        SDL_Log("Couldn't create texture: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+    SDL_DestroySurface(surface);
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
     if (event->type == SDL_EVENT_QUIT)
     {
-        return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
+        return SDL_APP_SUCCESS;
     }
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+
+    if (event->type == SDL_EVENT_MOUSE_WHEEL)
+    {
+        float alpha = SDL_GetWindowOpacity(window);
+        alpha += (event->wheel.y > .0f) ? +.1f : -.1f;
+        SDL_SetWindowOpacity(window, alpha);
+    }
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    const double now = ((double)SDL_GetTicks()) / 1000.0; /* convert from milliseconds to seconds. */
-    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
-    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
-
-    /* clear the window to the draw color. */
-    SDL_RenderClear(renderer);
-
-    /* put the newly-cleared rendering on the screen. */
+    SDL_RenderTexture(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer);
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    /* SDL will clean up the window/renderer for us. */
 }
