f19614e31a010271bd69b58929b4701d3388ec5c
DonizeteVida <donizetevida1998@outlook.com>
2025-06-26 00:01:11 +0200

    Add drag&move support

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
+static SDL_FRect texture_rect;
+static SDL_Point mouse_movement;
+static bool is_dragging = false;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -18,7 +21,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_SetHint(SDL_HINT_MAIN_CALLBACK_RATE, "15"))
+    if (!SDL_SetHint(SDL_HINT_MAIN_CALLBACK_RATE, "60"))
     {
         SDL_Log("Couldn't set framerate: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -49,6 +52,9 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     }
     SDL_DestroySurface(surface);
 
+    texture_rect.w = texture->w;
+    texture_rect.h = texture->h;
+
     return SDL_APP_CONTINUE;
 }
 
@@ -65,12 +71,36 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
         alpha += (event->wheel.y > .0f) ? +.1f : -.1f;
         SDL_SetWindowOpacity(window, alpha);
     }
+
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN && !is_dragging)
+    {
+        mouse_movement.x = event->motion.x;
+        mouse_movement.y = event->motion.y;
+
+        is_dragging = true;
+    }
+
+    if (event->type == SDL_EVENT_MOUSE_MOTION && is_dragging)
+    {
+        texture_rect.x += event->motion.x - mouse_movement.x;
+        texture_rect.y += event->motion.y - mouse_movement.y;
+
+        mouse_movement.x = event->motion.x;
+        mouse_movement.y = event->motion.y;
+    }
+
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_UP)
+    {
+        is_dragging = false;
+    }
+
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    SDL_RenderTexture(renderer, texture, NULL, NULL);
+    SDL_RenderClear(renderer);
+    SDL_RenderTexture(renderer, texture, NULL, &texture_rect);
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;
