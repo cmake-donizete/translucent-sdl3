31a7bab96f9cd0a0d9a4b0ec5e8e5a39d67fded9
DonizeteVida <donizetevida1998@outlook.com>
2025-06-28 15:24:51 +0200

    Add argument parsing

---
+++ include/main.h
@@ -0,0 +1,30 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+
+struct args
+{
+    float window_scale;
+    float image_scale;
+    char path[256];
+};
+
+struct state
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *texture;
+
+    SDL_FRect f_rect;
+    SDL_Point mouse_movement;
+
+    bool is_dragging;
+};
+
+static struct args args = {
+    .window_scale = 1.0f,
+    .image_scale = 1.0f,
+};
+
+static struct state state;
--- main.c
+++ main.c
@@ -1,20 +1,56 @@
+#include <main.h>
 
-#define SDL_MAIN_USE_CALLBACKS 1
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <SDL3_image/SDL_image.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
-static SDL_Texture *texture = NULL;
+static void parse_args(int argc, char *argv[])
+{
+    int opt, index;
 
-static SDL_FRect f_rect;
-static SDL_Point mouse_movement;
+    struct option long_options[] = {
+        {"window_scale", required_argument, 0, 'w'},
+        {"image_scale", required_argument, 0, 'i'},
+        {"path", required_argument, 0, 'p'},
+        {0, 0, 0, 0},
+    };
 
-static bool is_dragging = false;
+    while ((opt = getopt_long(argc, argv, "w:i:p:", long_options, &index)) != -1)
+    {
+        switch (opt)
+        {
+        case 'w':
+        {
+            args.window_scale = atof(optarg);
+            break;
+        }
+
+        case 'i':
+        {
+            args.image_scale = atof(optarg);
+            break;
+        }
+
+        case 'p':
+        {
+            strncpy(args.path, optarg, sizeof(args.path));
+            break;
+        }
+
+        default:
+        {
+            fprintf(stderr, "Unknown argument: %d", optopt);
+            exit(EXIT_FAILURE);
+            break;
+        }
+        }
+    }
+}
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
+    parse_args(argc, argv);
+
     SDL_Surface *surface = NULL;
 
     if (!SDL_Init(SDL_INIT_VIDEO))
@@ -29,7 +65,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!(surface = IMG_Load("sample.jpeg")))
+    if (!(surface = IMG_Load(args.path)))
     {
         SDL_Log("Couldn't load image: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -37,31 +73,36 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 
     if (!SDL_CreateWindowAndRenderer(
             "translucent",
-            surface->w,
-            surface->h,
-            SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_TRANSPARENT,
-            &window,
-            &renderer))
+            surface->w * args.window_scale,
+            surface->h * args.window_scale,
+            SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_TRANSPARENT | SDL_WINDOW_RESIZABLE,
+            &state.window,
+            &state.renderer))
     {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!(texture = SDL_CreateTextureFromSurface(renderer, surface)))
+    if (!(state.texture = SDL_CreateTextureFromSurface(state.renderer, surface)))
     {
         SDL_Log("Couldn't create texture: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     SDL_DestroySurface(surface);
 
-    f_rect.w = texture->w;
-    f_rect.h = texture->h;
+    state.f_rect.w = state.texture->w * args.image_scale;
+    state.f_rect.h = state.texture->h * args.image_scale;
 
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
+    SDL_Window *window = state.window;
+    SDL_Texture *texture = state.texture;
+    SDL_Point *mouse_movement = &state.mouse_movement;
+    SDL_FRect *f_rect = &state.f_rect;
+
     if (event->type == SDL_EVENT_QUIT)
     {
         return SDL_APP_SUCCESS;
@@ -81,42 +122,42 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     {
         float scale = (event->wheel.y > .0f) ? +.1f : -.1f;
 
-        float horizontal_percentage = mouse_movement.x / (float)texture->w;
-        float vertical_percentage = mouse_movement.y / (float)texture->h;
+        float horizontal_percentage = mouse_movement->x / (float)texture->w;
+        float vertical_percentage = mouse_movement->y / (float)texture->h;
 
-        float width = f_rect.w;
-        float height = f_rect.h;
+        float width = f_rect->w;
+        float height = f_rect->h;
 
-        f_rect.w += width * scale;
-        f_rect.h += height * scale;
+        f_rect->w += width * scale;
+        f_rect->h += height * scale;
 
-        f_rect.x -= (f_rect.w - width) / (1.0f / horizontal_percentage);
-        f_rect.y -= (f_rect.h - height) / (1.0f / vertical_percentage);
+        f_rect->x -= (f_rect->w - width) / (1.0f / horizontal_percentage);
+        f_rect->y -= (f_rect->h - height) / (1.0f / vertical_percentage);
     }
 
-    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN && !is_dragging)
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN && !state.is_dragging)
     {
-        mouse_movement.x = event->motion.x;
-        mouse_movement.y = event->motion.y;
+        mouse_movement->x = event->motion.x;
+        mouse_movement->y = event->motion.y;
 
-        is_dragging = true;
+        state.is_dragging = true;
     }
 
     if (event->type == SDL_EVENT_MOUSE_MOTION)
     {
-        if (is_dragging)
+        if (state.is_dragging)
         {
-            f_rect.x += event->motion.x - mouse_movement.x;
-            f_rect.y += event->motion.y - mouse_movement.y;
+            f_rect->x += event->motion.x - mouse_movement->x;
+            f_rect->y += event->motion.y - mouse_movement->y;
         }
 
-        mouse_movement.x = event->motion.x;
-        mouse_movement.y = event->motion.y;
+        mouse_movement->x = event->motion.x;
+        mouse_movement->y = event->motion.y;
     }
 
     if (event->type == SDL_EVENT_MOUSE_BUTTON_UP)
     {
-        is_dragging = false;
+        state.is_dragging = false;
     }
 
     return SDL_APP_CONTINUE;
@@ -124,9 +165,9 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    SDL_RenderClear(renderer);
-    SDL_RenderTexture(renderer, texture, NULL, &f_rect);
-    SDL_RenderPresent(renderer);
+    SDL_RenderClear(state.renderer);
+    SDL_RenderTexture(state.renderer, state.texture, NULL, &state.f_rect);
+    SDL_RenderPresent(state.renderer);
 
     return SDL_APP_CONTINUE;
 }
