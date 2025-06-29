1b53e631bfee22fe058555179603081117233464
DonizeteVida <donizetevida1998@outlook.com>
2025-06-29 19:38:51 +0200

    Add support to amd64/linux cross compile

--- toolchain-mingw64.cmake
+++ toolchain-mingw64.cmake
@@ -1,16 +1,12 @@
-# windows.cmake
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)  # or i686 for 32-bit
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# Specify the cross compiler
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
-# Specify the target environment
 set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
 
-# Adjust the default behavior of the find commands
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
